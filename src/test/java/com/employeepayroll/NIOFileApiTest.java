0dfd1b93511964f90c27cc00623d34ae05b90496
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-26 16:49:18 +0530

    Added: Watch service to watch particular directory along with all Files

--- src/test/java/com/employeepayroll/NIOFileApiTest.java
+++ src/test/java/com/employeepayroll/NIOFileApiTest.java
@@ -48,5 +48,12 @@ public class NIOFileApiTest {
         Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp")).forEach(System.out::println);
     }
 
+
+    @Test
+    public void givenDirectoryWhenWatchedListsAllTheActivities() throws IOException {
+        Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchServiceExample(dir).processEvents();
+    }
 }
 
